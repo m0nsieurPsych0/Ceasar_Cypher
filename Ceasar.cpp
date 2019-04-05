134c8783df8767ea8e21083f7c91692d412fb76b
Psycho <Psycho@UNKNOWN>
2019-04-05 12:46:21 -0400

    -User can enter any text -Will reproduce spaces and special characters.
    -Added option to do another transposition instead of automatically quitting the program.

--- Ceasar.cpp
+++ Ceasar.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
-//#include <conio.h>
 #include <iomanip>
-
+#include <conio.h>
 #include "console(v1.8).h"
 
+bool AnotherOperation();
+void ClearBuffer();
 
+
+
+void ClearBuffer()
+{
+	if (cin.fail()) cin.clear();			
+	cin.ignore(cin.rdbuf()->in_avail());	
+	while (_kbhit()) _getch();
+}
+
+bool AnotherOperation()
+{
+	char input;
+
+	cout << "\n\n\tDo you want to do an another rotation? 'Y' or 'N' ";
+		do 
+		{
+			ClearBuffer();
+			input = _getch();
+		} while (input != 'y' && input != 'n');
+		
+		if (input == 'y')
+			return true;
+		else
+			return false;
+}
 
 using namespace std;
 
 void main()
 {
 	/*Constantes*/
-	string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", Text, Version = "Ver: 1.3", 
+	string AlphabetUP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", AlphabetLOW = "abcdefghijklmnopqrstuvwxyz", Text, Version = "Ver: 2.0", 
 		Title = R"(
 		   ___                                   ___                _                 
 		  / __\  ___   __ _  ___   __ _  _ __   / __\ _   _  _ __  | |__    ___  _ __ 
@@ -23,7 +49,7 @@ void main()
 )";
 
 	/*Variables*/
-	unsigned int Rot;
+	unsigned int Rot, x, y;
 
 	/***************************Début du Programme*************************/
 	cvmSetColor(VERT);
@@ -32,31 +58,47 @@ void main()
 	cout << setw(100) << Version << endl << endl;
 	cvmResetColor();
 
-	cout << "\tText to encrypt : ";
-	cin >> Text;
-
-	cout << "\n\tNumber of rotations : ";
-	cin >> Rot;
-
-	cout << "\n\tResult	: ";
-
-	for (int j = 0; j < Text.size(); j++)
+	x = wherex();
+	y = wherey();
+	do
 	{
-		for (int i = 0; i < Alphabet.size(); i++)
-		{
-			
-			if(islower(Text[j]))
-			{
-				Text[j] = toupper(Text[j]);
-			}
-			if (Alphabet[i] == Text[j])
-			{
-				cout << Alphabet[(i + Rot) % 26];
-			}
-			
-		}
-	}
+		gotoxy(x, y);
+		clreoscr();
+
+		cout << "\tText to encrypt : ";
+		getline(cin, Text);
+
+		cout << "\n\tNumber of rotations : ";
+		cin >> Rot;
+
+		cout << "\n\tResult	: ";
+
 	
-	_getch();
-}
+	
+		for (int j = 0; j < Text.size(); j++)
+		{
+			for (int i = 0; i < 26 ; i++)
+			{
+				
+				if (AlphabetLOW[i] == Text[j])
+				{
+					cout << AlphabetLOW[(i + Rot) % 26];
+					break;
+				}
+				else if (AlphabetUP[i] == Text[j])
+				{
+					cout << AlphabetUP[(i + Rot) % 26];
+					break;
+				}
+				else if (i == 25)
+				{
+					cout << Text[j];
+				}
+			}
+
+		}
+		
+	} while (AnotherOperation());
+	
+}//main
 
